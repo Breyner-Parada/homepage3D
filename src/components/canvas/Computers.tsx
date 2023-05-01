fdcc1ee38126952065ae9ef2220e4af0d00eb454
Breyner Parada <paradabre@gmail.com>
2023-04-30 20:44:36 -0500

    Path desktop_pc fixed

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -8,7 +8,7 @@ type ComputerProps = {
 };
 
 const Computer = ({ isMobile }: ComputerProps): JSX.Element => {
-  const computer = useGLTF("../../../public/desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
